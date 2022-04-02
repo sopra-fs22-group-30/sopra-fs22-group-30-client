3a1642b485016244fe77721d481d85dd1fffb941
DuanHuiran <570989207@qq.com>
2022-04-02 14:38:33 +0200

    left column of profile page

--- src/components/views/Profile_recipes.js
+++ src/components/views/Profile_recipes.js
@@ -1,19 +1,36 @@
 import "styles/views/Profile.scss";
+import profile_photo from 'profile_photo.svg';
 
-const Profile_recipes = (props) => {
-    return(
-        <div className="profile column left">
+const Photo = () => {
+    return (
+        <img src={profile_photo} className="profile photo" alt="profile_photo"/>
+    )
+}
 
-        </div>
+const Username = () => {
+    return (
+        <h2>fakeUsername</h2>
+    )
+}
+
+const MyLikesButton = (props) => {
+    return (
+        <div className="myLikes-button">My Likes</div>
     )
 }
 
 
+const Profile_recipes = (props) => {
+    return (
+        <div className="profile column left">
+            <Photo/>
+            <Username/>
+            <MyLikesButton/>
+            <h3><span className="line"></span> My Recipes <span className="line"></span></h3>
+        </div>
+    )
 
-
-
-
-
+}
 
 
 export default Profile_recipes;
