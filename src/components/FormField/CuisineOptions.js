c2967aac021da4b9c4b4987536bf8621c07bbede
DuanHuiran <570989207@qq.com>
2022-04-10 22:53:01 +0200

    add cuisine options

--- src/components/FormField/CuisineOptions.js
+++ src/components/FormField/CuisineOptions.js
@@ -1,7 +1,3 @@
-const cuisineOptions = [
-    "Chinese",
-    "Italian",
-    "Dessert"
-];
+const cuisineOptions = ['Algerian', 'American', 'Belgian', 'Brazilian', 'British', 'Cajun', 'Canadian', 'Chinese', 'Cuban', 'Egyptian', 'French', 'German', 'Greek', 'Haitian', 'Hawaiian', 'Indian', 'Irish', 'Italian', 'Japanese', 'Jewish', 'Kenyan', 'Korean', 'Latvian', 'Libyan', 'Mexican', 'Mormon', 'Nigerian', 'Peruvian', 'Polish', 'Portuguese', 'Russian', 'Salvadorian', 'Scottish', 'Spanish', 'Swedish', 'Tahitian', 'Thai', 'Tibetan', 'Turkish', 'Welsh'];
 
 export default cuisineOptions;
